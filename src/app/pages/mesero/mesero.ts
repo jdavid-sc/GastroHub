1dad9b62c2d232b4d07054fedf38d8fd9f29f5da
jose usuga <jose1028.m@gmail.com>
2026-04-05 22:52:21 -0500

    feat: precios con formato moneda y limpieza de código

--- src/app/pages/mesero/mesero.ts
+++ src/app/pages/mesero/mesero.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule, DecimalPipe } from '@angular/common';
+import { CommonModule, } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 interface Product {
@@ -18,7 +18,7 @@ interface OrderItem {
 @Component({
   selector: 'app-mesero',
   standalone: true,
-  imports: [CommonModule, FormsModule, DecimalPipe],
+  imports: [CommonModule, FormsModule,],
   templateUrl: './mesero.html',
   styleUrls: ['./mesero.css']
 })
@@ -28,16 +28,16 @@ export class MeseroComponent {
 
   // 10 Platos reales con imágenes
   products: Product[] = [
-    { id: 1, name: 'Margarita Tradicional', price: 28.00, image: 'https://images.unsplash.com/photo-1574071318508-1cdbab80d002?w=500&q=80' },
-    { id: 2, name: 'Risotto de Setas', price: 22.00, image: 'https://images.unsplash.com/photo-1473093295043-cdd812d0e601?w=500&q=80' },
-    { id: 3, name: 'Tacos de Atún', price: 14.00, image: 'https://images.unsplash.com/photo-1551504734-5ee1c4a1479b?w=500&q=80' },
-    { id: 4, name: 'Muffin de Arándanos', price: 8.00, image: 'https://images.unsplash.com/photo-1607958996333-41aef7caefaa?w=500&q=80' },
-    { id: 5, name: 'Hamburguesa Clásica', price: 18.00, image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=500&q=80' },
-    { id: 6, name: 'Ensalada César', price: 12.00, image: 'https://images.unsplash.com/photo-1550304943-4f24f54ddde9?w=500&q=80' },
-    { id: 7, name: 'Corte Ribeye (Carnes)', price: 45.00, image: 'https://images.unsplash.com/photo-1594046243098-0fceea9d451e?w=500&q=80' },
-    { id: 8, name: 'Sushi Roll Salmón', price: 24.00, image: 'https://images.unsplash.com/photo-1579871494447-9811cf80d66c?w=500&q=80' },
-    { id: 9, name: 'Sopa de Tomate', price: 9.00, image: 'https://images.unsplash.com/photo-1547592166-23ac45744acd?w=500&q=80' },
-    { id: 10, name: 'Cheesecake Fresa', price: 11.00, image: 'https://images.unsplash.com/photo-1533134242443-d4fd215305ad?w=500&q=80' }
+    { id: 1, name: 'Margarita Tradicional', price: 50000, image: 'https://images.unsplash.com/photo-1574071318508-1cdbab80d002?w=500&q=80' },
+    { id: 2, name: 'Risotto de Setas', price: 45000, image: 'https://images.unsplash.com/photo-1473093295043-cdd812d0e601?w=500&q=80' },
+    { id: 3, name: 'Tacos de Atún', price: 34000, image: 'https://images.unsplash.com/photo-1551504734-5ee1c4a1479b?w=500&q=80' },
+    { id: 4, name: 'Muffin de Arándanos', price: 28000, image: 'https://images.unsplash.com/photo-1607958996333-41aef7caefaa?w=500&q=80' },
+    { id: 5, name: 'Hamburguesa Clásica', price: 52000, image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=500&q=80' },
+    { id: 6, name: 'Ensalada César', price: 25000, image: 'https://images.unsplash.com/photo-1550304943-4f24f54ddde9?w=500&q=80' },
+    { id: 7, name: 'Corte Ribeye (Carnes)', price:75000, image: 'https://images.unsplash.com/photo-1594046243098-0fceea9d451e?w=500&q=80' },
+    { id: 8, name: 'Sushi Roll Salmón', price: 64000, image: 'https://images.unsplash.com/photo-1579871494447-9811cf80d66c?w=500&q=80' },
+    { id: 9, name: 'Sopa de Tomate', price: 20000, image: 'https://images.unsplash.com/photo-1547592166-23ac45744acd?w=500&q=80' },
+    { id: 10, name: 'Cheesecake Fresa', price: 19000, image: 'https://images.unsplash.com/photo-1533134242443-d4fd215305ad?w=500&q=80' }
   ];
 
   // Lógica para que la barra de búsqueda filtre al instante
